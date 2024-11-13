ed44d7bd7721bfd76eb63510ad4a6017908fb95a
Brady Six <bsixdev@gmail.com>
2024-11-12 18:41:59 -0800

    refactor: mutate request

---
+++ src/main/java/com/bsix/healthio/workout/WorkoutMutateBody.java
@@ -0,0 +1,18 @@
+package com.bsix.healthio.workout;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import java.time.Instant;
+
+public record WorkoutMutateBody(
+    @PastOrPresent(message = "You cannot log future workouts!") Instant date,
+    @NotNull
+        @Min(value = 1, message = "Your workout must burn at least 1 calorie!")
+        @Max(value = 9999, message = "Your workout cannot burn more than 9,999 calories!")
+        Integer caloriesBurned,
+    @NotNull
+        @Min(value = 1, message = "Your workout must be at least 1 minute long!")
+        @Max(value = 1440, message = "Your workout cannot exceed 24 hours!")
+        Integer durationMinutes) {}
