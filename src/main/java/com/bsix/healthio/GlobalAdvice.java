a397af26648ebc750997777240f2c964edeb9035
Brady Six <bsixdev@gmail.com>
2024-11-12 19:10:12 -0800

    feat: add global controller advice

--- src/main/java/com/bsix/healthio/GlobalAdvice.java
+++ src/main/java/com/bsix/healthio/GlobalAdvice.java
@@ -1,9 +1,12 @@
 package com.bsix.healthio;
 
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalAdvice {
@@ -12,4 +15,34 @@ public class GlobalAdvice {
     ProblemDetail handleErrorResponseException(ErrorResponseException e) {
         return e.getBody();
     }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  ProblemDetail handleConstraintViolationException(ConstraintViolationException e) {
+    HttpStatusCode code = HttpStatusCode.valueOf(400);
+
+    ProblemDetail pd = ProblemDetail.forStatus(code);
+
+    pd.setTitle("Constraint Violation Detected");
+
+    var violation = e.getConstraintViolations().stream().findAny();
+
+    pd.setDetail(violation.isPresent() ? violation.get().getMessage() : "");
+
+    pd.setDetail(e.getConstraintViolations().stream().findAny().get().getMessage());
+    return handleErrorResponseException(new ErrorResponseException(code, pd, e));
+  }
+
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  ProblemDetail handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+    HttpStatusCode code = HttpStatusCode.valueOf(400);
+
+    ProblemDetail pd = ProblemDetail.forStatus(code);
+
+    pd.setTitle("Argument Type Mismatch");
+
+    pd.setDetail(
+        e.getParameter().getParameterName() + " could not be handled as the expected type.");
+
+    return handleErrorResponseException(new ErrorResponseException(code, pd, e));
+  }
 }
