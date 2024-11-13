c744f130ca4b9fd43388e70a60e6316b1f1afe13
Brady Six <bsixdev@gmail.com>
2024-11-13 11:13:50 -0800

    test: workout flow

--- src/test/java/com/bsix/healthio/workout/WorkoutTest.java
+++ src/test/java/com/bsix/healthio/workout/WorkoutTest.java
@@ -1,14 +1,29 @@
 package com.bsix.healthio.workout;
 
+import static com.bsix.healthio.workout.WorkoutController.ROOT_URI;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import com.bsix.healthio.TestConfig;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.*;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestConfig.class)
 public class WorkoutTest {
 
   static final Sort DEFAULT_WORKOUT_DATE_SORT = Sort.by(Sort.Order.desc("date"));
@@ -50,4 +65,175 @@ public class WorkoutTest {
 
   public static final Page<Workout> DEFAULT_WORKOUT_PAGE =
       new PageImpl<>(DEFAULT_WORKOUT_LIST, DEFAULT_WORKOUT_PAGEABLE, DEFAULT_WORKOUT_LIST.size());
+
+  @Autowired private TestRestTemplate http;
+
+  @Autowired private WorkoutRepository workoutRepository;
+
+  private List<Workout> workouts;
+
+  @BeforeEach
+  void beforeEach() {
+    workouts = workoutRepository.saveAll(DEFAULT_WORKOUT_LIST);
+  }
+
+  @Test
+  void getWorkouts() {
+    var req =
+        RequestEntity.get(ROOT_URI)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer token")
+            .accept(MediaType.APPLICATION_JSON)
+            .build();
+
+    var ref = new ParameterizedTypeReference<PagedModel<EntityModel<Workout>>>() {};
+
+    var res = http.exchange(req, ref);
+
+    assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(res.getHeaders().containsKey(HttpHeaders.ALLOW));
+    assertThat(res.getBody().getMetadata().getSize()).isNotNegative();
+    assertThat(res.getBody().getMetadata().getNumber()).isNotNegative();
+    assertThat(res.getBody().getMetadata().getTotalPages()).isNotNegative();
+    assertThat(res.getBody().getMetadata().getTotalElements()).isNotNegative();
+    assertThat(res.getBody().getContent().size()).isPositive();
+  }
+
+  @Test
+  void getWorkouts_WithBadParams_ShouldReturnProblemDetail() {
+    var uri =
+        UriComponentsBuilder.fromUriString(ROOT_URI)
+            .queryParam("dateStart", Instant.now().plusSeconds(3600).toString())
+            .build()
+            .toUri();
+
+    var req =
+        RequestEntity.get(uri)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer token")
+            .accept(MediaType.APPLICATION_JSON)
+            .build();
+
+    var ref = new ParameterizedTypeReference<ProblemDetail>() {};
+
+    var res = http.exchange(req, ref);
+
+    assertThat(res.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    assertThat(res.getHeaders().containsKey(HttpHeaders.ALLOW));
+    assertThat(res.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_PROBLEM_JSON);
+  }
+
+  @Test
+  void postWorkout() {
+    var uri = UriComponentsBuilder.fromUriString(ROOT_URI).build().toUri();
+
+    var mut = new WorkoutMutateBody(null, 350, 32);
+
+    var req =
+        RequestEntity.post(uri)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer token")
+            .accept(MediaType.APPLICATION_JSON)
+            .body(mut);
+
+    var ref = new ParameterizedTypeReference<EntityModel<Workout>>() {};
+
+    var res = http.exchange(req, ref);
+
+    assertThat(res.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    assertThat(res.getHeaders().containsKey(HttpHeaders.ALLOW));
+    assertThat(res.getHeaders().containsKey(HttpHeaders.LOCATION));
+    assertThat(res.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_JSON);
+    assertThat(res.getBody().hasLink(IanaLinkRelations.SELF));
+    assertThat(res.getBody().getContent().getDate()).isNotNull();
+  }
+
+  @Test
+  void postWorkout_WithBadRequest_ShouldReturnProblemDetail() {
+    var uri = UriComponentsBuilder.fromUriString(ROOT_URI).build().toUri();
+
+    var mut = new WorkoutMutateBody(Instant.EPOCH.minusSeconds(3600), -235, -32);
+
+    var req =
+        RequestEntity.post(uri)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer token")
+            .accept(MediaType.APPLICATION_JSON)
+            .body(mut);
+
+    var ref = new ParameterizedTypeReference<ProblemDetail>() {};
+
+    var res = http.exchange(req, ref);
+
+    assertThat(res.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    assertThat(res.getHeaders().containsKey(HttpHeaders.ALLOW));
+    assertThat(res.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_PROBLEM_JSON);
+  }
+
+  @Test
+  void putWorkout() {
+    var id = workouts.get(0).getId();
+
+    var uri = UriComponentsBuilder.fromUriString(ROOT_URI + "/" + id).build().toUri();
+
+    var mut = new WorkoutMutateBody(null, 320, 30);
+
+    var req =
+        RequestEntity.put(uri)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer token")
+            .accept(MediaType.APPLICATION_JSON)
+            .body(mut);
+
+    var res = http.exchange(req, Void.class);
+
+    assertThat(res.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    assertThat(res.getHeaders().containsKey(HttpHeaders.ALLOW));
+    assertThat(workoutRepository.findById(id).get().getDate()).isNotNull();
+  }
+
+  @Test
+  void putWorkout_WithBadRequest_ShouldReturnProblemDetail() {
+    var id = workouts.get(0).getId();
+
+    var uri = UriComponentsBuilder.fromUriString(ROOT_URI + "/" + id).build().toUri();
+
+    var mut = new WorkoutMutateBody(Instant.EPOCH.minusSeconds(12 * 2592000), -320, -30);
+
+    var req =
+        RequestEntity.put(uri)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer token")
+            .accept(MediaType.APPLICATION_JSON)
+            .body(mut);
+
+    var res = http.exchange(req, ProblemDetail.class);
+
+    assertThat(res.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    assertThat(res.getHeaders().containsKey(HttpHeaders.ALLOW));
+    assertThat(res.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_PROBLEM_JSON);
+  }
+
+  @Test
+  void deleteWorkout() {
+    var id = workouts.get(0).getId();
+
+    var uri = UriComponentsBuilder.fromUriString(ROOT_URI + "/" + id).build().toUri();
+
+    var req = RequestEntity.delete(uri).header(HttpHeaders.AUTHORIZATION, "Bearer token").build();
+
+    var res = http.exchange(req, Void.class);
+
+    assertThat(res.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    assertThat(res.getHeaders().containsKey(HttpHeaders.ALLOW));
+  }
+
+  @Test
+  void deleteWorkout_WithUnknownId_ShouldReturnProblemDetail() {
+    var id = UUID.randomUUID();
+
+    var uri = UriComponentsBuilder.fromUriString(ROOT_URI + "/" + id).build().toUri();
+
+    var req = RequestEntity.delete(uri).header(HttpHeaders.AUTHORIZATION, "Bearer token").build();
+
+    var res = http.exchange(req, ProblemDetail.class);
+
+    assertThat(res.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    assertThat(res.getHeaders().containsKey(HttpHeaders.ALLOW));
+    assertThat(res.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_PROBLEM_JSON);
+  }
 }
