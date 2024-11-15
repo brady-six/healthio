7c16601f16a03aac2ced510f1f72a3cd8ee26019
Brady Six <bsixdev@gmail.com>
2024-11-15 09:32:53 -0800

    feat: update security config

--- src/main/java/com/bsix/healthio/SecurityConfig.java
+++ src/main/java/com/bsix/healthio/SecurityConfig.java
@@ -4,16 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
   @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http.authorizeHttpRequests(
-            req -> req.requestMatchers("/api").authenticated().anyRequest().permitAll())
-        .oauth2ResourceServer(s -> s.jwt(Customizer.withDefaults()))
-        .build();
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(c -> c.anyRequest().authenticated())
+        .csrf().disable()
+        .oauth2Login(Customizer.withDefaults())
+        .oauth2Client(Customizer.withDefaults())
+        .oauth2ResourceServer(c -> c.jwt(Customizer.withDefaults()));
+
+    return http.build();
   }
 }
