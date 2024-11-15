817498a36fc6df459c7515bee51c39ce57c57109
Brady Six <bsixdev@gmail.com>
2024-11-15 14:09:58 -0800

    fix: make prod-specific security filter

--- src/main/java/com/bsix/healthio/SecurityConfig.java
+++ src/main/java/com/bsix/healthio/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.bsix.healthio;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,12 +10,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Profile("prod")
 public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(c -> c.anyRequest().authenticated())
-        .csrf().disable()
+        .csrf(Customizer.withDefaults())
         .oauth2Login(Customizer.withDefaults())
         .oauth2Client(Customizer.withDefaults())
         .oauth2ResourceServer(c -> c.jwt(Customizer.withDefaults()));
